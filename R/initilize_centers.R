d20ef7d26abcfe4432e02f6204d73e7bca11022c
Douglas Carmona <douglas.albertocg@gmail.com>
2022-12-19 22:36:30 -0300

    Change: Replace R's robinden function  for c++'s

--- R/initilize_centers.R
+++ R/initilize_centers.R
@@ -1,26 +1,24 @@
-KMEANS_K_DEFAULT = 20
-ROBIN_DEFAULT_METHOD = "density"
+KMEANS_K_DEFAULT <- 20
+ROBIN_DEFAULT_METHOD <- "density"
 
-init_kmeans = function(data, K) {
-  ret = kmeans(data, K, nstart = KMEANS_K_DEFAULT)$centers
+init_kmeans <- function(data, K) {
+  ret <- kmeans(data, K, nstart = KMEANS_K_DEFAULT)$centers
   return(ret)
 }
 
-init_random = function(data, K) {
-  ret = data[sample(nrow(data), K), ]
+init_random <- function(data, K) {
+  ret = data[sample(1:nrow(data), K), ]
   return(ret)
 }
 
-init_robin = function(data, K) {
-  ret_rob <- robust_init_density(dist_matrix = dist(data), 
-                                 data = data, 
-                                 n_clusters = K, 
-                                 method = ROBIN_DEFAULT_METHOD)
-  ret = data[ret_rob$centers, ]
+init_robin <- function(data, K) {
+  ret_rob <- robinden(distance(data), K, 10)
+  r_centers <- ret_rob$centers + 1
+  ret <- data[r_centers, ]
   return(ret)
 }
 
-init_custom = function(data, K) {
-  ret = K
+init_custom <- function(data, K) {
+  ret <- K
   return(ret)
 }
